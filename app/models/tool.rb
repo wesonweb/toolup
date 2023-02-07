eeb77826b5c4ff627d553230f0b14fbff40ab833
Kenneth So <kso117@gmail.com>
2023-02-07 20:16:05 +0000

    create tool table

---
+++ app/models/tool.rb
@@ -0,0 +1,3 @@
+class Tool < ApplicationRecord
+  belongs_to :user_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_164735) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_201234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tools", force: :cascade do |t|
+    t.string "type"
+    t.integer "price"
+    t.string "location"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tools_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_164735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tools", "users"
 end
