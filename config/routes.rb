49809e8b9388b5cbd361feb61faeb47aab952b1e
Zuzanna Szymczak <zuzannaolgaszymczak@gmail.com>
2023-02-09 20:23:50 +0000

    test changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
-Rails.application.routes.draw do
-  resources :tools
+Rails.application.routes.draw doß
   devise_for :users
   root to: "pages#home"
+  resources :tools, except: [:edit, :update] do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: :create
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
