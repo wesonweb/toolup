4dfbe93fa36ba52af1c24fb6108c9b32172d3458
Aysegul Cakir <aysegul.cakir@kcl.ac.uk>
2023-02-09 20:45:17 +0000

    changes on the view booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
+Require "Time"
 class BookingsController < ApplicationController
   before_action :set_booking, only: :destroy
   before_action :set_tool, only: [:new, :create]
-
 
   def index
     @bookings = Booking.all
@@ -14,9 +14,9 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.tool = @tool
-    @booking.date= Date.now
+    @booking.date = Date.now
     if @booking.save
-      redirect_to tool_path(@tool)
+      redirect_to  tool_bookings_path(@tool)
     else
       render :new, status: :unprocessable_entity
     end
@@ -24,13 +24,13 @@ class BookingsController < ApplicationController
 
   def destroy
     @booking.destroy
-    redirect_to tool_path(@booking.list), status: :see_other
+    redirect_to booking_path, status: :see_other
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:date, :start_date, :end_date,:status, :user_id, :tool_id )
+    params.require(:booking).permit(:date, :start_date, :end_date, :status, :user_id, :tool_id)
   end
 
   def set_booking
