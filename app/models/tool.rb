e63353208e04bdf7eabf8f3e322fe36373cca1cd
Kenneth So <kso117@gmail.com>
2023-02-09 18:33:04 +0000

    add tool validate

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,10 @@
 class Tool < ApplicationRecord
+  TYPES = ["Electrical", "Sewing & Cutting", "Woodworking", "Measuring", "Plumbing"]
+
   belongs_to :user
+  has_many :booking
+
+  validates :price, :location, description, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates :type, inclusion: { in: TYPES }
 end
