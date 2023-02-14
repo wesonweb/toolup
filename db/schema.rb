447ed8377f500612a9d29d26c367470cd8449b76
Wes Mehaffy <wesonweb@gmail.com>
2023-02-14 19:44:59 +0000

    add geocoder gem and add lat and long cols to tools table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_210354) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_194349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_210354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_tools_on_user_id"
   end
 
