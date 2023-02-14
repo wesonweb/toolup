bb20c6d43b402775b39c1dada4ed5a6a1d5006bc
Wes Mehaffy <wesonweb@gmail.com>
2023-02-14 14:17:55 +0000

    updates booking confirm show page and adds logo to navbar

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :tool
+
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    return unless end_date < start_date
+
+    errors.add(:end_date, "must be after the start date")
+  end
 end
