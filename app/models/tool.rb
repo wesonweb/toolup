e73c09515650de666622cec0637ada0e4da0ca79
Wes Mehaffy <wesonweb@gmail.com>
2023-02-16 19:30:35 +0000

    updates model and bookings to fix reviews bug

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,11 +1,12 @@
 class Tool < ApplicationRecord
   NAMES = ["Circular saw", "Jigsaw", "Drill", "Clamp", "Lawnmower", "Leaf blower", "Sander"]
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  # has_many :review, through: :booking, dependnt: :destroy
   validates :price, :address, presence: true
   validates :price, numericality: { only_integer: true }
   validates :name, inclusion: { in: NAMES }
